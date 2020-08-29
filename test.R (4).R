9c59ba01dcbbd7b0b2bfff1b76927a72e608c816
dimple-rawat <70286363+dimple-rawat@users.noreply.github.com>
2020-08-29 12:22:50 +0530

    Add files via upload

---
+++ test.R (4).R
@@ -0,0 +1,25 @@
+x<-58
+y<-20
+z<-100
+x*y*z
+#create vector
+my<-c(12,20,15,100)
+a<-c(1,2,3,5)
+a*5
+a+(a+1)
+vec<-c('r','pyth',5/9,30)
+vec
+
+#how not to declare  vector
+#bb<-c[10,11,12]
+#bb
+
+#data frame
+df<-data.frame(x=c(1,2,3),y=c("a","b","c"))
+df[1,3,2]
+df[1,]
+df[c(1,3),2]
+
+
+#slicing of data frame
+df
